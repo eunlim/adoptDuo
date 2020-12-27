c0a356c8613f59ad737cd0ca202d22156999b7cd
subin <subin@61.77.116.22>
2020-12-27 17:16:44 +0900

    user

--- src/main/java/org/project/signt/controller/home/HomeController.java
+++ src/main/java/org/project/signt/controller/home/HomeController.java
@@ -4,8 +4,6 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 
 import org.project.signt.service.home.HomeService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
