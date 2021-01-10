64be53c6e1ce53e3293677159e48bf930335355f
user <user@61.77.118.142>
2021-01-10 16:25:08 +0900

    dddddd

--- src/main/java/org/project/signt/controller/myPage/MyPageController.java
+++ src/main/java/org/project/signt/controller/myPage/MyPageController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 /**
  * Handles requests for the application home page.
  */
